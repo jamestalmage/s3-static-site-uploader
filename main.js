bae7184d7f407f6124467de4974938cda37d8c72
jamestalmage <james.talmage@jrtechnical.com>
2013-06-25 15:35:15 -0400

    Upload working - crazy fast

--- main.js
+++ main.js
@@ -43,69 +43,7 @@ Constructor.prototype = {
     }
 };
 
-var createParams = {
-    createBucket:function(bucketName){
-        return {
-            Bucket: bucketName
-        };
-    },
-    /*
-     *  exists and is readable - promise resolves
-     *  does not exist - promise rejected with code 404 (NotFound)
-     *  exists and is forbidden - 403(Forbidden)
-     */
-    headBucket:function(bucketName){
-        return {
-            Bucket:bucketName
-        };
-    },
-    putBucketPolicy:function(bucketName,policy){
-        var policyString = JSON.stringify(policy);
-
-        return {
-            Bucket:bucketName,
-            Policy:policyString
-        };
-    },
-    putObject:function(bucketName, key, body,mimeType){
-        mimeType = mimeType || mime.lookup(key);
-
-       // console.log(body);
-        return {
-            Bucket:bucketName,
-            Key: key,
-            Body: body,//new Buffer(body),
-            ContentType: mimeType
-        };
-    },
-    putBucketWebsite:function(bucketName,index,error) {
-        var params = {
-            Bucket:bucketName,
-            WebsiteConfiguration:{}
-        };
-        if(index){
-            params.WebsiteConfiguration.IndexDocument = {
-                Suffix:index
-            };
-        }
-        if(error){
-            params.WebsiteConfiguration.ErrorDocument = {
-                Key:error
-            };
-        }
-        return params;
-    },
-
-    listObjects:function(bucketName,prefix){
-        var params = {
-            Bucket:bucketName
-        };
-        if(prefix){
-            params.Prefix = prefix;
-        }
-        return params;
-    }
-};
+var createParams = require('./lib/s3-parameter.js');
 
 function addParameterFunction(functionName){
     Constructor.prototype[functionName] = function(){
