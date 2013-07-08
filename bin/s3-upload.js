4c5e47027e808d05444165a8e1210e43ba9d30f8
jamestalmage <james.talmage@jrtechnical.com>
2013-07-08 16:56:13 -0400

    Preparing to push to npm

---
+++ bin/s3-upload.js
@@ -0,0 +1,13 @@
+var ConfigRunner = require('../src/ConfigRunner.js');
+var path = require('path');
+
+var runner = new ConfigRunner();
+
+
+var configPath = path.resolve('./aws-upload.conf.js');
+
+var config = require(configPath);
+
+runner.setConfig(config);
+
+runner.run();
